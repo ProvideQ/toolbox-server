27fe34bbf9f98729198be037a90212903cdb92b7
Max Schweikart <hello@maxschweik.art>
2022-09-29 15:47:31 +0200

    initialize toolbox server project

---
+++ src/main/java/edu/kit/provideq/toolbox/ToolboxServerApplication.java
@@ -0,0 +1,13 @@
+package edu.kit.provideq.toolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolboxServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ToolboxServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/kit/provideq/toolbox/ToolboxServerApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kit.provideq.toolbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolboxServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
