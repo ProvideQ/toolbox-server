2e75dd563b13b79fc1da4ca0736d1b3c0bcbba85
PhilippSeidel00 <spam@philipp-seidel.de>
2022-12-08 18:30:28 +0100

    added stub for the QiskitMaxCutSolver

--- src/main/java/edu/kit/provideq/toolbox/meta/ProblemType.java
+++ src/main/java/edu/kit/provideq/toolbox/meta/ProblemType.java
@@ -8,5 +8,11 @@ public enum ProblemType {
    * A satisfiability problem:
    * For a given boolean formula, check if there is an interpretation that satisfies the formula.
    */
-  SAT
+  SAT,
+
+  /**
+   * An optimization problem:
+   * For a given graph, find the optimal separation of vertices that maximises the cut crossing edge weight sum.
+   */
+  MAX_CUT
 }
