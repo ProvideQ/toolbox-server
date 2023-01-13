39b5cad6cf43252f8f07c41ae8d1968e8c2fa57f
nickp <nickposer2102@gmail.com>
2023-01-13 15:15:54 +0100

    Refactored Request data structure so automatic http json parsing works

--- src/main/java/edu/kit/provideq/toolbox/SolveRequest.java
+++ src/main/java/edu/kit/provideq/toolbox/SolveRequest.java
@@ -3,10 +3,6 @@ package edu.kit.provideq.toolbox;
 /**
  * Abstract solve request containing some request content
  */
-public abstract class SolveRequest<RequestType> {
-  public final RequestType requestContent;
-
-  public SolveRequest(RequestType requestContent) {
-    this.requestContent = requestContent;
-  }
+public class SolveRequest<RequestType> {
+  public RequestType requestContent;
 }
--- src/main/java/edu/kit/provideq/toolbox/maxCut/SolveMaxCutRequest.java
+++ src/main/java/edu/kit/provideq/toolbox/maxCut/SolveMaxCutRequest.java
@@ -6,8 +6,4 @@ import edu.kit.provideq.toolbox.SolveRequest;
  * POST Requests to /solve/maxCut should have a response body of this form.
  * The needed formula the maxCut formula to solve in the GML format.
  */
-public class SolveMaxCutRequest extends SolveRequest<String> {
-  public SolveMaxCutRequest(String formula) {
-    super(formula);
-  }
-}
+public class SolveMaxCutRequest extends SolveRequest<String> {}
