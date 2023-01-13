dcb7eb1c242a1111cd68c1022a817b99d9488b31
nickp <nickposer2102@gmail.com>
2023-01-13 15:15:02 +0100

    Added CrossOrigin annotation which seems to be required

--- src/main/java/edu/kit/provideq/toolbox/sat/SatController.java
+++ src/main/java/edu/kit/provideq/toolbox/sat/SatController.java
@@ -6,17 +6,14 @@ import edu.kit.provideq.toolbox.meta.MetaSolver;
 import edu.kit.provideq.toolbox.sat.solvers.SATSolver;
 import javax.validation.Valid;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SatController extends ProblemController<String, String, SATSolver> {
 
   private final MetaSolver<SATSolver> metaSolver = new MetaSolverSAT();
 
+  @CrossOrigin
   @PostMapping("/solve/sat")
   public SolutionHandle solveSat(@RequestBody @Valid SolveSatRequest request) {
     return super.solve(request, metaSolver);
