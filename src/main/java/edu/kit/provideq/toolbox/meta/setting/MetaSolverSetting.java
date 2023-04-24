bcaab6d85a22c43280f51383656c5fcc6adeba71
nickp <nickposer2102@gmail.com>
2023-04-24 23:37:21 +0200

    Add json deserializer for meta solver setting

--- src/main/java/edu/kit/provideq/toolbox/meta/setting/MetaSolverSetting.java
+++ src/main/java/edu/kit/provideq/toolbox/meta/setting/MetaSolverSetting.java
@@ -1,5 +1,8 @@
 package edu.kit.provideq.toolbox.meta.setting;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+@JsonDeserialize(using = MetaSolverSettingDeserializer.class)
 public abstract class MetaSolverSetting {
     public String name;
     public MetaSolverSettingType type;
