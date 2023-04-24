4877f4f1fe1ac10fc060f84e341106f4e19048f0
nickp <nickposer2102@gmail.com>
2023-04-24 23:37:01 +0200

    Add meta solver settings to meta solver finding process

--- src/main/java/edu/kit/provideq/toolbox/SolveRequest.java
+++ src/main/java/edu/kit/provideq/toolbox/SolveRequest.java
@@ -1,8 +1,10 @@
 package edu.kit.provideq.toolbox;
 
 import edu.kit.provideq.toolbox.meta.ProblemType;
+import edu.kit.provideq.toolbox.meta.setting.MetaSolverSetting;
 import org.springframework.lang.Nullable;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -17,12 +19,16 @@ public class SolveRequest<RequestType> {
   public String requestedSolverId;
 
   @Nullable
+  public List<MetaSolverSetting> requestedMetaSolverSettings;
+
+  @Nullable
   public Map<ProblemType, SolveRequest> requestedSubSolveRequests;
 
   public <T> SolveRequest<T> replaceContent(T otherContent) {
     var request = new SolveRequest<T>();
     request.requestContent = otherContent;
     request.requestedSolverId = requestedSolverId;
+    request.requestedMetaSolverSettings = requestedMetaSolverSettings;
     request.requestedSubSolveRequests = requestedSubSolveRequests;
 
     return request;
--- src/main/java/edu/kit/provideq/toolbox/featureModel/anomaly/MetaSolverFeatureModelAnomaly.java
+++ src/main/java/edu/kit/provideq/toolbox/featureModel/anomaly/MetaSolverFeatureModelAnomaly.java
@@ -2,11 +2,13 @@ package edu.kit.provideq.toolbox.featureModel.anomaly;
 
 import edu.kit.provideq.toolbox.meta.MetaSolver;
 import edu.kit.provideq.toolbox.meta.Problem;
+import edu.kit.provideq.toolbox.meta.setting.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import edu.kit.provideq.toolbox.featureModel.anomaly.solvers.*;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 /**
@@ -21,7 +23,7 @@ public class MetaSolverFeatureModelAnomaly extends MetaSolver<FeatureModelAnomal
   }
 
   @Override
-  public FeatureModelAnomalySolver findSolver(Problem problem) {
+  public FeatureModelAnomalySolver findSolver(Problem problem, List<MetaSolverSetting> metaSolverSettings) {
     // todo add decision
     return (new ArrayList<>(this.solvers)).get((new Random()).nextInt(this.solvers.size()));
   }
