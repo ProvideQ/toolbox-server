eae411e6b5a765eb7c135bc41742493042e11b4c
nickp <nickposer2102@gmail.com>
2023-05-03 11:32:36 +0200

    Use arguments instead of Object[]

--- src/test/java/edu/kit/provideq/toolbox/format/DimacsCNFTests.java
+++ src/test/java/edu/kit/provideq/toolbox/format/DimacsCNFTests.java
@@ -2,6 +2,7 @@ package edu.kit.provideq.toolbox.format;
 
 import edu.kit.provideq.toolbox.format.cnf.dimacs.DimacsCNF;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,59 +21,59 @@ public class DimacsCNFTests {
         assertEquals(result, dimacsCNF.toString());
     }
 
-    static Stream<Object[]> testToStringParameters() {
+    static Stream<Arguments> testToStringParameters() {
         return Stream.of(
-                new Object[]{"(!A | B)", """
-                                c A 2
-                                c B 1
-                                p cnf 2 1
-                                1 -2 0
-                                """},
-                new Object[]{"!A | B", """
-                                c A 2
-                                c B 1
-                                p cnf 2 1
-                                1 -2 0
-                                """},
-                new Object[]{"not A or B", """
-                                c A 2
-                                c B 1
-                                p cnf 2 1
-                                1 -2 0
-                                """},
-                new Object[]{"(!A | B) & !C & D", """
-                                c A 4
-                                c B 3
-                                c C 2
-                                c D 1
-                                p cnf 4 3
-                                1 0
-                                -2 0
-                                3 -4 0
-                                """},
-                new Object[]{"(A | B) & C", """
-                                c A 2
-                                c B 3
-                                c C 1
-                                p cnf 3 2
-                                1 0
-                                2 3 0
-                                """},
-                new Object[]{"""
-                                c C 1
-                                c A 2
-                                c B 3
-                                p cnf 3 2
-                                1 0
-                                2 -3 0
-                                """, """
-                                c C 1
-                                c A 2
-                                c B 3
-                                p cnf 3 2
-                                1 0
-                                2 -3 0
-                                """}
+                Arguments.arguments("(!A | B)", """
+                        c A 2
+                        c B 1
+                        p cnf 2 1
+                        1 -2 0
+                        """),
+                Arguments.arguments("!A | B", """
+                        c A 2
+                        c B 1
+                        p cnf 2 1
+                        1 -2 0
+                        """),
+                Arguments.arguments("not A or B", """
+                        c A 2
+                        c B 1
+                        p cnf 2 1
+                        1 -2 0
+                        """),
+                Arguments.arguments("(!A | B) & !C & D", """
+                        c A 4
+                        c B 3
+                        c C 2
+                        c D 1
+                        p cnf 4 3
+                        1 0
+                        -2 0
+                        3 -4 0
+                        """),
+                Arguments.arguments("(A | B) & C", """
+                        c A 2
+                        c B 3
+                        c C 1
+                        p cnf 3 2
+                        1 0
+                        2 3 0
+                        """),
+                Arguments.arguments("""
+                        c C 1
+                        c A 2
+                        c B 3
+                        p cnf 3 2
+                        1 0
+                        2 -3 0
+                        """, """
+                        c C 1
+                        c A 2
+                        c B 3
+                        p cnf 3 2
+                        1 0
+                        2 -3 0
+                        """)
         );
     }
 }
