2b011e6866776acf129a0093a7ca9657df6d23cc
nickp <nickposer2102@gmail.com>
2023-05-03 21:31:27 +0200

    Fix getBeans creation, from default singleton to one new bean per request

--- src/main/java/edu/kit/provideq/toolbox/SubRoutinePool.java
+++ src/main/java/edu/kit/provideq/toolbox/SubRoutinePool.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.function.Function;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class SubRoutinePool {
     private final Map<ProblemType, SolveRequest> subRoutineCalls;
 
