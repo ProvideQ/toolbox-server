f7e9aff1eb32780e6f181c939e6bff7e7a788c07
nickp <nickposer2102@gmail.com>
2023-07-08 14:34:32 +0200

    Add Variable documentation

--- src/main/java/edu/kit/provideq/toolbox/format/cnf/dimacs/Variable.java
+++ src/main/java/edu/kit/provideq/toolbox/format/cnf/dimacs/Variable.java
@@ -2,7 +2,19 @@ package edu.kit.provideq.toolbox.format.cnf.dimacs;
 
 import java.util.Objects;
 
+/**
+ * Represents a variable in a DIMACS CNF file.
+
+ * @param number number of the variable
+ * @param name name of the variable
+ * @param isNegated true if the variable is negated
+ */
 public record Variable(int number, String name, boolean isNegated) {
+  /**
+   * Copy constructor.
+
+   * @param other variable to copy
+   */
   public Variable(Variable other) {
     this(other.number, other.name, other.isNegated);
   }
