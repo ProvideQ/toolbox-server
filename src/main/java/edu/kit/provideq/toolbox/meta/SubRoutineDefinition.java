52b69080f5104d385dff515b1c2575d5cc21b444
nickp <nickposer2102@gmail.com>
2023-09-11 16:30:35 +0200

    Change SubRoutineDefinition to just use ProblemType

--- src/main/java/edu/kit/provideq/toolbox/meta/SubRoutineDefinition.java
+++ src/main/java/edu/kit/provideq/toolbox/meta/SubRoutineDefinition.java
@@ -4,21 +4,10 @@ package edu.kit.provideq.toolbox.meta;
  * A sub-routine definition describes which problem type needs to be solved by a sub-routine and why
  * it needs to be solved.
  *
- * @param problemTypeId {@link ProblemType#getId() id} of the problem type that needs to be solved
+ * @param type {@link ProblemType} that needs to be solved
  *     by this sub-routine.
  * @param description description of the sub-routine call to provide information where and why it is
  *     needed.
- * @see #SubRoutineDefinition(ProblemType, String)
  */
-public record SubRoutineDefinition(String problemTypeId, String description) {
-  /**
-   * Creates a sub-routine definition for a given problem type with a given description.
-   *
-   * @param type problem type that needs to be solved by this sub-routine.
-   * @param description description of the sub-routine call to provide information where and why it
-   *     is needed.
-   */
-  public SubRoutineDefinition(ProblemType type, String description) {
-    this(type.getId(), description);
-  }
+public record SubRoutineDefinition(ProblemType type, String description) {
 }
