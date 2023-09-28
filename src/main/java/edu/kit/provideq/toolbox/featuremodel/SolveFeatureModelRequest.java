cb424475cea2b0c71c2c5cf5cb8ea7c3bfe85e17
nickp <nickposer2102@gmail.com>
2023-09-28 23:16:31 +0200

    Change response body to request body

--- src/main/java/edu/kit/provideq/toolbox/featuremodel/SolveFeatureModelRequest.java
+++ src/main/java/edu/kit/provideq/toolbox/featuremodel/SolveFeatureModelRequest.java
@@ -3,7 +3,7 @@ package edu.kit.provideq.toolbox.featuremodel;
 import edu.kit.provideq.toolbox.SolveRequest;
 
 /**
- * POST Requests to /solve/featureModel should have a response body of this form.
+ * POST Requests to /solve/featureModel should have a request body of this form.
  * The needed formula the featureModel formula to solve in the DIMACS CNF - SAT format.
  */
 public class SolveFeatureModelRequest extends SolveRequest<String> {
--- src/main/java/edu/kit/provideq/toolbox/maxcut/SolveMaxCutRequest.java
+++ src/main/java/edu/kit/provideq/toolbox/maxcut/SolveMaxCutRequest.java
@@ -3,7 +3,7 @@ package edu.kit.provideq.toolbox.maxcut;
 import edu.kit.provideq.toolbox.SolveRequest;
 
 /**
- * POST Requests to /solve/max-cut should have a response body of this form.
+ * POST Requests to /solve/max-cut should have a request body of this form.
  * The needed formula the maxcut formula to solve in the GML format.
  */
 public class SolveMaxCutRequest extends SolveRequest<String> {
