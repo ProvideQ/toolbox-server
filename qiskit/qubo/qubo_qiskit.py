8d44733a97e221a8eaf5a0bd49396437dbc8dca4
nickp <nickposer2102@gmail.com>
2023-09-28 17:20:00 +0200

    Remove old code

--- qiskit/qubo/qubo_qiskit.py
+++ qiskit/qubo/qubo_qiskit.py
@@ -1,4 +1,3 @@
-import copy
 import sys
 
 from qiskit.algorithms.minimum_eigensolvers import QAOA
@@ -6,7 +5,6 @@ from qiskit.algorithms.optimizers import COBYLA
 from qiskit.primitives import Sampler
 from qiskit_optimization import QuadraticProgram
 from qiskit_optimization.algorithms import MinimumEigenOptimizer
-from qiskit_optimization.problems import VarType
 
 if len(sys.argv) != 3:
     raise TypeError('This script expects exactly 2 arguments. Input file (argument 1) and output file (argument 2).')
@@ -17,24 +15,11 @@ output_path = sys.argv[2]
 qp = QuadraticProgram()
 qp.read_from_lp_file(input_path)
 
-
-def relax_problem(problem):
-    """Change all variables to continuous."""
-    relaxed_problem = copy.deepcopy(problem)
-    for variable in relaxed_problem.variables:
-        variable.vartype = VarType.CONTINUOUS
-
-    return relaxed_problem
-
-qubo = qp
-# qubo = relax_problem(QuadraticProgramToQubo().convert(qp))
-# print(qp.prettyprint())
-
 qaoa_mes = QAOA(Sampler(), optimizer=COBYLA(), initial_point=[0.0, 1.0])
 
 qaoa = MinimumEigenOptimizer(qaoa_mes)
 
-qaoa_result = qaoa.solve(qubo)
+qaoa_result = qaoa.solve(qp)
 print(qaoa_result.prettyprint())
 
 f = open(output_path, 'w')
