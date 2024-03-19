94c1452f2fd618844211c8094f18ed29213745d8
Lucas Berger <lucask.berger@gmail.com>
2024-03-19 18:08:40 +0100

    feat: add first test to vrp solver

---
+++ src/test/java/edu/kit/provideq/toolbox/api/VRPSolverTest.java
@@ -0,0 +1,105 @@
+package edu.kit.provideq.toolbox.api;
+
+import static edu.kit.provideq.toolbox.SolutionStatus.SOLVED;
+import static org.hamcrest.Matchers.is;
+
+import edu.kit.provideq.toolbox.process.BinaryProcessRunner;
+import edu.kit.provideq.toolbox.qubo.QuboMetaSolver;
+import edu.kit.provideq.toolbox.qubo.solvers.DwaveQuboSolver;
+import edu.kit.provideq.toolbox.qubo.solvers.QiskitQuboSolver;
+import edu.kit.provideq.toolbox.qubo.solvers.QrispQuboSolver;
+import edu.kit.provideq.toolbox.MetaSolverProvider;
+import edu.kit.provideq.toolbox.ResourceProvider;
+import edu.kit.provideq.toolbox.Solution;
+import edu.kit.provideq.toolbox.SubRoutinePool;
+import edu.kit.provideq.toolbox.vrp.MetaSolverVrp;
+import edu.kit.provideq.toolbox.vrp.SolveVrpRequest;
+import edu.kit.provideq.toolbox.vrp.clusterer.KmeansClusterer;
+import edu.kit.provideq.toolbox.vrp.clusterer.MetaSolverClusterVrp;
+import edu.kit.provideq.toolbox.vrp.clusterer.NoClusteringClusterer;
+import edu.kit.provideq.toolbox.vrp.clusterer.TwoPhaseClusterer;
+import edu.kit.provideq.toolbox.vrp.solvers.ClusterAndSolveVrpSolver;
+import edu.kit.provideq.toolbox.vrp.solvers.LkhVrpSolver;
+import edu.kit.provideq.toolbox.vrp.solvers.QuboTspSolver;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@WebFluxTest
+@Import(value = {
+    SolveRouter.class,
+    MetaSolverClusterVrp.class,
+    MetaSolverProvider.class,
+    MetaSolverVrp.class,
+    ClusterAndSolveVrpSolver.class,
+    BinaryProcessRunner.class,
+    LkhVrpSolver.class,
+    QuboTspSolver.class,
+    SubRoutinePool.class,
+    ResourceProvider.class,
+    NoClusteringClusterer.class,
+    TwoPhaseClusterer.class,
+    KmeansClusterer.class,
+    QuboMetaSolver.class,
+    QiskitQuboSolver.class,
+    QrispQuboSolver.class,
+    DwaveQuboSolver.class
+})
+class VRPSolverTest {
+  @Autowired
+  private WebTestClient client;
+
+  @Autowired
+  private MetaSolverVrp metaSolverVrp;
+
+  Stream<Arguments> provideArguments() {
+    String exampleProblem = metaSolverVrp.getExampleProblems().get(0);
+
+    var simpleLKHRequest = new SolveVrpRequest();
+
+    simpleLKHRequest.requestedSolverId = LkhVrpSolver.class.getName();
+    simpleLKHRequest.requestContent = exampleProblem;
+
+    
+    return List.of(simpleLKHRequest).stream().map(Arguments::of);
+  }
+
+  @BeforeEach
+  void setUp() {  
+    this.client = this.client
+                             .mutate()
+                             .responseTimeout(Duration.ofSeconds(10)) // Set to 10 seconds
+                             .build();
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideArguments")
+  void testVrpSolver(SolveVrpRequest request) {
+
+    var response = client.post()
+        .uri("/solve/vrp")
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(request)
+        .exchange();
+
+
+    response.expectStatus().isOk();
+    response.expectBody(new ParameterizedTypeReference<Solution<String>>() {
+        })
+        .value(Solution::getStatus, is(SOLVED));
+  }
+}
