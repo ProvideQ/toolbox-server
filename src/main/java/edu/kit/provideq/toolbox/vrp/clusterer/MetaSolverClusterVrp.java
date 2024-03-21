13b35c6e81c815ceed9d0933afbdfa387e964ceb
Lucas Berger <lucask.berger@gmail.com>
2024-03-21 17:27:23 +0100

    fix: sonar probelms

--- src/main/java/edu/kit/provideq/toolbox/vrp/clusterer/MetaSolverClusterVrp.java
+++ src/main/java/edu/kit/provideq/toolbox/vrp/clusterer/MetaSolverClusterVrp.java
@@ -12,7 +12,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.Random;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
