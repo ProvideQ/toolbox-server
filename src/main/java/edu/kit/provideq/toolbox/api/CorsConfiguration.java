93c75c1d171f89582caa2fcafd28cd0f9a66cdce
Elscrux <nickposer2102@gmail.com>
2024-07-30 15:34:29 +0200

    feat: Allow additional http methods

--- src/main/java/edu/kit/provideq/toolbox/api/CorsConfiguration.java
+++ src/main/java/edu/kit/provideq/toolbox/api/CorsConfiguration.java
@@ -14,6 +14,7 @@ public class CorsConfiguration implements WebFluxConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
-        .allowedOrigins("*");
+            .allowedOrigins("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
   }
 }
