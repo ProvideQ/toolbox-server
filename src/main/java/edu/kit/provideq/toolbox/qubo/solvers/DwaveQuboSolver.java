c06fd676d7b3bbcef8187facff7af2aa9441b507
Elscrux <nickposer2102@gmail.com>
2024-09-06 17:17:05 +0200

    feat: Add dwave annealing setting

--- src/main/java/edu/kit/provideq/toolbox/qubo/solvers/DwaveQuboSolver.java
+++ src/main/java/edu/kit/provideq/toolbox/qubo/solvers/DwaveQuboSolver.java
@@ -3,9 +3,13 @@ package edu.kit.provideq.toolbox.qubo.solvers;
 import edu.kit.provideq.toolbox.Solution;
 import edu.kit.provideq.toolbox.meta.SolvingProperties;
 import edu.kit.provideq.toolbox.meta.SubRoutineResolver;
+import edu.kit.provideq.toolbox.meta.setting.SolverSetting;
+import edu.kit.provideq.toolbox.meta.setting.basic.SelectSetting;
+import edu.kit.provideq.toolbox.meta.setting.basic.TextSetting;
 import edu.kit.provideq.toolbox.process.ProcessRunner;
 import edu.kit.provideq.toolbox.process.PythonProcessRunner;
 import edu.kit.provideq.toolbox.qubo.QuboConfiguration;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -18,6 +22,27 @@ import reactor.core.publisher.Mono;
  */
 @Component
 public class DwaveQuboSolver extends QuboSolver {
+  private static final String SETTING_DWAVE_TOKEN = "D-Wave Token";
+  private static final String SETTING_ANNNEALING_METHOD = "Annealing Method";
+  private static final AnnealingMethod DEFAULT_ANNEALING_METHOD = AnnealingMethod.SIMULATED;
+
+  enum AnnealingMethod {
+    SIMULATED("sim"),
+    HYBRID("hybrid"),
+    ABSOLV("absolv"),
+    DIRECT("direct");
+
+    private final String value;
+
+    AnnealingMethod(String value) {
+      this.value = value;
+    }
+
+    public String getValue() {
+      return value;
+    }
+  }
+
   private final String scriptPath;
   private final ApplicationContext context;
 
@@ -35,34 +60,51 @@ public class DwaveQuboSolver extends QuboSolver {
   }
 
   @Override
+  public List<SolverSetting> getSolverSettings() {
+    return  List.of(
+        new TextSetting(
+            SETTING_DWAVE_TOKEN,
+            "The D-Wave token to use, only relevant when a token is added"
+        ),
+        new SelectSetting<>(
+            SETTING_ANNNEALING_METHOD,
+            "The annealing method to use, only relevant when a token is added",
+            List.of(AnnealingMethod.values()),
+            AnnealingMethod.SIMULATED
+        )
+    );
+  }
+
+  @Override
   public Mono<Solution<String>> solve(
       String input,
       SubRoutineResolver subRoutineResolver,
       SolvingProperties properties
   ) {
 
-    // there is currently no field where a token can be added by the user
-    // this field is kept because it was used in Lucas implementation and will be added back later
-    // TODO: add user setting that allows passing Tokens
-    Optional<String> dwaveToken = Optional.empty();
+    Optional<String> dwaveToken = properties
+        .<TextSetting>getSetting(SETTING_DWAVE_TOKEN)
+        .map(TextSetting::getText);
 
     // this field is only relevant when a dwaveToken is added
     // (a token is needed to access the d-wave hardware)
-    // options are: sim, hybrid, absolv, direct
-    String dwaveAnnealingMethod = "sim";
+    var dwaveAnnealingMethod = properties
+        .<SelectSetting<AnnealingMethod>>getSetting(SETTING_ANNNEALING_METHOD)
+        .map(SelectSetting::getSelectedOption)
+        .orElse(DEFAULT_ANNEALING_METHOD);
 
     var solution = new Solution<>(this);
 
     var processRunner = context.getBean(PythonProcessRunner.class, scriptPath);
 
-    if (dwaveToken.isPresent()) {
+    if (dwaveToken.isPresent() && !dwaveToken.get().isEmpty()) {
       processRunner.withEnvironmentVariable("DWAVE_API_TOKEN", dwaveToken.get());
     }
 
     var processResult = processRunner
         .withArguments(
             ProcessRunner.INPUT_FILE_PATH,
-            dwaveAnnealingMethod,
+            dwaveAnnealingMethod.value,
             "--output-file", ProcessRunner.OUTPUT_FILE_PATH
         )
         .withInputFile(input, "problem.lp")
