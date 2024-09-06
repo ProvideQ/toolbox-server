f35938f362fc0fb4608465b76d5145f02eb09fe7
Elscrux <nickposer2102@gmail.com>
2024-09-06 13:20:19 +0200

    refactor: Rename setting classes and move them to a nested package

--- src/main/java/edu/kit/provideq/toolbox/qubo/solvers/QrispQuboSolver.java
+++ src/main/java/edu/kit/provideq/toolbox/qubo/solvers/QrispQuboSolver.java
@@ -3,7 +3,8 @@ package edu.kit.provideq.toolbox.qubo.solvers;
 import edu.kit.provideq.toolbox.Solution;
 import edu.kit.provideq.toolbox.meta.SolvingProperties;
 import edu.kit.provideq.toolbox.meta.SubRoutineResolver;
-import edu.kit.provideq.toolbox.meta.setting.BoundedInteger;
+import edu.kit.provideq.toolbox.process.ProcessRunner;
+import edu.kit.provideq.toolbox.meta.setting.basic.IntegerSetting;
 import edu.kit.provideq.toolbox.meta.setting.SolverSetting;
 import edu.kit.provideq.toolbox.process.PythonProcessRunner;
 import edu.kit.provideq.toolbox.qubo.QuboConfiguration;
@@ -41,7 +42,7 @@ public class QrispQuboSolver extends QuboSolver {
   @Override
   public List<SolverSetting> getSolverSettings() {
     return List.of(
-        new BoundedInteger(
+        new IntegerSetting(
             SETTING_MAX_NUMBER_OF_VARS,
             "The maximum number of variables that can be passed to the QAOA solver"
                 + " which will affect the size of the gate.",
@@ -63,8 +64,8 @@ public class QrispQuboSolver extends QuboSolver {
     // This value will be passed to the python script,
     // it is used to prevent denial of service issues for large simulations.
     // Default value is 4, higher values are possible but might take much longer to simulate.
-    int maxNumberOfVariables = properties.<BoundedInteger>getSetting(SETTING_MAX_NUMBER_OF_VARS)
-        .map(BoundedInteger::getValue)
+    int maxNumberOfVariables = properties.<IntegerSetting>getSetting(SETTING_MAX_NUMBER_OF_VARS)
+        .map(IntegerSetting::getValue)
         .orElse(DEFAULT_MAX_NUMBER_OF_VARS);
 
     if (maxNumberOfVariables < 1) {
