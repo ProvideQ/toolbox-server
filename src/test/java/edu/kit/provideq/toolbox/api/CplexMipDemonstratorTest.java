133e24dcbae724ce6dbeee39ce4fc05e9bf490ba
Elscrux <nickposer2102@gmail.com>
2024-10-11 13:25:47 +0200

    feat: Add cplex mip test

---
+++ src/test/java/edu/kit/provideq/toolbox/api/CplexMipDemonstratorTest.java
@@ -0,0 +1,43 @@
+package edu.kit.provideq.toolbox.api;
+
+import static edu.kit.provideq.toolbox.demonstrators.DemonstratorConfiguration.DEMONSTRATOR;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import edu.kit.provideq.toolbox.SolutionStatus;
+import edu.kit.provideq.toolbox.demonstrators.CplexMipDemonstrator;
+import edu.kit.provideq.toolbox.meta.ProblemState;
+import java.time.Duration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@SpringBootTest
+@AutoConfigureMockMvc
+class CplexMipDemonstratorTest {
+  @Autowired
+  private WebTestClient client;
+
+  @Autowired
+  private CplexMipDemonstrator cplexMipDemonstrator;
+
+  @BeforeEach
+  void beforeEach() {
+    this.client = this.client.mutate()
+        .responseTimeout(Duration.ofSeconds(20))
+        .build();
+  }
+
+  @Test
+  void testCplexMipDemonstrator() {
+    var problem = ApiTestHelper.createProblem(client, cplexMipDemonstrator, "", DEMONSTRATOR);
+    assertEquals(ProblemState.SOLVED, problem.getState());
+    assertNotNull(problem.getSolution());
+    assertEquals(SolutionStatus.SOLVED, problem.getSolution().getStatus());
+  }
+}
