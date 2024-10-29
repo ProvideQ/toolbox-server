c6e73b392daca14efd52ae6f7aaf6b16e324d92f
piotr.malkowski <piotr.malkowski@student.kit.edu>
2024-10-29 12:57:43 +0100

    Remove limit of 1-deep-subdirectory search

--- scripts/install-python-dependencies.py
+++ scripts/install-python-dependencies.py
@@ -5,12 +5,15 @@ script_dir = os.path.dirname(os.path.realpath(__file__))
 base_dir = os.path.join(script_dir, '..', 'solvers')
 
 for root, dirs, files in os.walk(base_dir):
-    if root == base_dir:  # iterate only one level deep
-        for directory in dirs:
-            req_file = os.path.join(base_dir, directory, 'requirements.txt')
-            if os.path.exists(req_file):
-                print(f"Found requirements.txt in {directory}, installing dependencies...")
-                try:
-                    subprocess.run(['pip', 'install', '-r', req_file], check=True)  # single thread to avoid dep. errors
-                except subprocess.CalledProcessError as e:
-                    print(f"Error installing requirements for {directory}: {e}")
+    # Check if 'requirements.txt' is in the current directory
+    req_file = os.path.join(root, 'requirements.txt')
+    if os.path.exists(req_file):
+        print(f"Found requirements.txt in {root}, installing dependencies...")
+        try:
+            subprocess.run(['pip', 'install', '-r', req_file], check=True) # single thread to avoid dep. errors
+        except subprocess.CalledProcessError as e:
+            print(f"Error installing requirements for {root}: {e}")
+
+
+
+
