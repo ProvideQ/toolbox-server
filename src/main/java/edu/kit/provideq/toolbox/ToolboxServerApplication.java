36f21b5ece597ced83c50ca889809b6c00227c88
Elscrux <nickposer2102@gmail.com>
2025-01-25 18:04:43 +0100

    Revert "feat: add more logging to ensure logs are working"
    This reverts commit eb9008efca1069d31f7b029228119e6e86fd64d2.

--- src/main/java/edu/kit/provideq/toolbox/ToolboxServerApplication.java
+++ src/main/java/edu/kit/provideq/toolbox/ToolboxServerApplication.java
@@ -1,15 +1,12 @@
 package edu.kit.provideq.toolbox;
 
 import edu.kit.provideq.toolbox.exception.MissingSpringProfileException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.stream.Stream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToolboxServerApplication {
-  private static final Logger logger = Logger.getLogger(ToolboxServerApplication.class.getName());
 
   public static void main(String[] args) throws MissingSpringProfileException {
     String springProfileArg = "--spring.profiles.active=" + getSpringProfileForOs();
@@ -19,9 +16,7 @@ public class ToolboxServerApplication {
         Stream.of(springProfileArg))
         .toArray(String[]::new);
 
-    logger.log(Level.INFO, "Starting Toolbox with Spring Profile: {0}", springProfileArg);
     SpringApplication.run(ToolboxServerApplication.class, mergedArgs);
-    logger.info("Toolbox started");
   }
 
   private static String getSpringProfileForOs() throws MissingSpringProfileException {
