960a6a8c99b6ad50cdd7302cdac2aec220679085
Piotr Malkowski <uixik@student.kit.edu>
2025-02-03 10:16:49 +0100

    chore: remove todo

--- src/test/java/edu/kit/provideq/toolbox/api/SharpSatSolversTest.java
+++ src/test/java/edu/kit/provideq/toolbox/api/SharpSatSolversTest.java
@@ -37,7 +37,6 @@ class SharpSatSolversTest {
 
   @SuppressWarnings("OptionalGetWithoutIsPresent")
   Stream<Arguments> provideArguments() {
-    // TODO: Investigate element not found
     var problemManager = problemManagerProvider.findProblemManagerForType(SHARPSAT).get();
 
     return ApiTestHelper.getAllArgumentCombinations(problemManager)
