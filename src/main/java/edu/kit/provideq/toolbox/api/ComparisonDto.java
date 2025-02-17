23dd65e12152fa278370bfb4fce5ce799dacd196
zaibod <uxoik@student.kit.edu>
2025-02-17 09:30:16 +0100

    refactor: problem bound structure

--- src/main/java/edu/kit/provideq/toolbox/api/ComparisonDto.java
+++ src/main/java/edu/kit/provideq/toolbox/api/ComparisonDto.java
@@ -3,9 +3,44 @@ package edu.kit.provideq.toolbox.api;
 import edu.kit.provideq.toolbox.BoundWithInfo;
 import edu.kit.provideq.toolbox.Solution;
 
-public record ComparisonDto(float comparison, BoundWithInfo bound, Solution<?> solution) {
-  @Override public String toString() {
-    return "Comparison{comparison=%f, bound=%s, solution=%s}"
-        .formatted(comparison, bound, solution);
+public class ComparisonDto {
+  private BoundDto bound;
+  private float comparison;
+
+  public ComparisonDto(float comparison, BoundWithInfo bound) {
+    this.comparison = comparison;
+    this.bound = new BoundDto(bound);
   }
+
+  public ComparisonDto() {
+    this.comparison = -1;
+    this.bound = null;
+  }
+
+
+  @Override public String toString() {
+    return "Comparison{bound=%s, comparison=%f}"
+        .formatted(bound, comparison);
+  }
+
+  public BoundDto getBound() {
+    return bound;
+  }
+
+  public void setBound(BoundDto bound) {
+    this.bound = bound;
+  }
+
+  public void setBound(BoundWithInfo bound) {
+    this.bound = new BoundDto(bound);
+  }
+
+  public void setComparison(float comparison) {
+    this.comparison = comparison;
+  }
+
+  public float getComparison() {
+    return comparison;
+  }
+
 }
