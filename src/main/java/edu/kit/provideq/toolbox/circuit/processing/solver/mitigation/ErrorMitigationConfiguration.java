a7f335e7286f5e38707f2304cb9c563d312d39c2
ekulos-code <admin@ekulos.de>
2025-03-10 13:28:22 +0100

    Merge dev, account for checkstyle errors

--- src/main/java/edu/kit/provideq/toolbox/circuit/processing/solver/mitigation/ErrorMitigationConfiguration.java
+++ src/main/java/edu/kit/provideq/toolbox/circuit/processing/solver/mitigation/ErrorMitigationConfiguration.java
@@ -1,22 +1,18 @@
 package edu.kit.provideq.toolbox.circuit.processing.solver.mitigation;
 
-import edu.kit.provideq.toolbox.circuit.processing.solver.executor.ExecutionResult;
-import edu.kit.provideq.toolbox.circuit.processing.solver.executor.ExecutionSolver;
 import edu.kit.provideq.toolbox.meta.Problem;
 import edu.kit.provideq.toolbox.meta.ProblemManager;
 import edu.kit.provideq.toolbox.meta.ProblemType;
+import java.util.Set;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Set;
 
 @Configuration
 public class ErrorMitigationConfiguration {
   public static final ProblemType<String, String> MITIGATION_CONFIG = new ProblemType<>(
       "circuit-processing-mitigation",
       String.class,
-      String.class,
-      null
+      String.class
   );
 
   @Bean
