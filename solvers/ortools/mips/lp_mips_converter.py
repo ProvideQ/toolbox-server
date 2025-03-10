93d038cc922db6e578f9fe0993e0853c5e18ce36
Piotr Malkowski <piotrmalkowski@protonmail.com>
2025-03-10 11:45:46 +0100

    feat: add CPLEX converter from and to .lp and .mps files

---
+++ solvers/ortools/mips/lp_mips_converter.py
@@ -0,0 +1,11 @@
+import cplex
+
+
+def convert_model(input_file, output_file):
+    """
+    Loads a model (LP or MPS) into CPLEX and writes it back out
+    in the requested format (.lp or .mps).
+    """
+    # Create a CPLEX object and read in the model
+    model = cplex.Cplex(input_file)
+    model.write(output_file)
