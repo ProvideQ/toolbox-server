d36e66fa09406f76ef0b39148c2a8886496f9c2d
ekulos-code <admin@ekulos.de>
2025-03-10 12:27:27 +0100

    Add execution and optimization solvers

--- src/main/java/edu/kit/provideq/toolbox/circuit/processing/CircuitProcessingConfiguration.java
+++ src/main/java/edu/kit/provideq/toolbox/circuit/processing/CircuitProcessingConfiguration.java
@@ -25,6 +25,24 @@ public class CircuitProcessingConfiguration {
       MoveToOptimizationSolver moveToOptimizationSolver,
       MoveToMitigationSolver moveToMitigationSolver
   ) {
+    Problem<String, String> demo = new Problem<>(CIRCUIT_PROCESSING);
+    demo.setInput("""
+        OPENQASM 2.0;
+        include "qelib1.inc";
+        qreg q[2];
+        creg c[2];
+        h q[0];
+        cx q[0],q[1];
+        measure q[0] -> c[0];
+        measure q[1] -> c[1];""");
+    Problem<String, String> secondDemo = new Problem<>(CIRCUIT_PROCESSING);
+    secondDemo.setInput("""
+        OPENQASM 2.0;
+        include "qelib1.inc";
+        qreg q[3];
+        crz(0.5) q[0], q[1];
+        t q[2];
+        cswap q[2], q[0], q[1];""");
     return new ProblemManager<>(
         CIRCUIT_PROCESSING,
         Set.of(
@@ -32,8 +50,7 @@ public class CircuitProcessingConfiguration {
             moveToOptimizationSolver,
             moveToMitigationSolver
         ),
-        // TODO: dummy problem
-        Set.of(new Problem<>(CIRCUIT_PROCESSING))
+        Set.of(demo, secondDemo)
     );
   }
 }
